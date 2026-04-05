3eb1181325ca9a46ec9b517bae5a31f6080f6adc
mmdc84br <mmdc84br@gmail.com>
2026-04-05 00:23:43 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// O Service Worker pode ser deixado vazio para testes básicos de instalação,
+// ou você pode adicionar cache para funcionar offline depois.
+self.addEventListener('fetch', (event) => {
+    // Aqui poderíamos interceptar as requisições
+});
