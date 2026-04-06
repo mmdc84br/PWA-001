1caaeac748c7a2e53831f0e4e3c185e6d0d0fd32
Donizetet <dtsp75@google.com>
2026-04-05 23:27:19 -0300

    atualização geral

--- 10 - PWA 01/sw.js
+++
@@ -1,5 +0,0 @@
-// O Service Worker pode ser deixado vazio para testes básicos de instalação,
-// ou você pode adicionar cache para funcionar offline depois.
-self.addEventListener('fetch', (event) => {
-    // Aqui poderíamos interceptar as requisições
-});
